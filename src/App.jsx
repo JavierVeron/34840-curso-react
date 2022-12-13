b5054a26305a62b15060f82cc3f8c72fbc95d716
Javier Veron <javier.veron@gmail.com>
2022-12-12 22:32:48 -0300

    Clase 8

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import Productos from './components/Clase7/Productos';
 import NavBar from './components/Clase7/NavBar';
 import Error404 from './components/Clase7/Error404';
 import Item from './components/Clase7/Item';
+import Eventos from './components/Clase8/Eventos';
+import Intercambiabilidad from './components/Clase8/Intercambiabilidad';
+import Abstraccion1 from './components/Clase8/Abstraccion1';
+import Abstraccion2 from './components/Clase8/Abstraccion2';
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route exact path={"/"} element={<Productos />} />
+        <Route exact path={"/"} element={<Abstraccion2 />} />
         <Route exact path={"/productos"} element={<Productos />} />
         <Route path={"/categoria/:categoryId"} element={<Productos />} />
         <Route path={"/item/:id"} element={<Item />} />
---
+++ src/components/Clase8/Eventos.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useEffect } from "react";
+
+const Eventos = () => {
+    const hacerClick = (event) => {
+        console.log(event.nativeEventj);
+    }
+
+    const eventoInput = (event) => {
+        event.preventDefault();
+        console.log("Evento Input");
+    }
+
+    const onClick = (event) => {
+        event.stopPropagation();
+        console.log("Stop Propagation");
+    }
+
+    /* const entradaAlBoton = () => {
+        console.log("Entraste al Botón!");
+    }
+
+     const salidaDelBoton = () => {
+        console.log("Saliste del Botón!");
+    }
+
+    const onResize = () => {
+        console.log("Hiciste un resize!");
+    }
+
+    window.addEventListener("resize", onResize);
+
+    useEffect(() => {
+        return () => {
+            window.removeEventListener("resize", onResize);
+        }
+    }, []); */
+
+    return (
+        <div className="container">
+            <h1>Eventos</h1>
+            <button className="btn btn-primary" onClick={(e) => {hacerClick(e)}}>Click</button><br /><input type="text" onKeyDown={eventoInput} /><br /><button className="btn btn-primary" onClick={onClick}>Click 2</button>
+        </div>
+    )
+}
+
+export default Eventos;
